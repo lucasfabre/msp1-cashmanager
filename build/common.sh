14375e4a8ef41ecb48b579122eefdcefc5f30e05
Lucas Fabre <fabre.lucas@outlook.com>
2019-10-08 16:18:17 +0200

    ci(android): add test and build apk in ./build/application

---
+++ build/common.sh
@@ -0,0 +1,44 @@
+set -e
+
+source $DIR/.env
+
+TEST=0
+PACKAGE=0
+BUILD_DOCKER=0
+KEEP_CACHE=0
+BUILD_CACHE_VOLUME=cashmanager-buildcache
+DOCKER_RUN_ARGS="--rm -v $BUILD_CACHE_VOLUME:/root/ -v $DIR:/app -it"
+
+GID=$(id -g)
+
+for i in "$@"
+do
+    case $i in
+        help)
+            print_help
+            exit 0
+        ;;
+        test)
+            TEST=1
+        ;;
+        package)
+            PACKAGE=1
+        ;;
+        build-docker)
+            BUILD_DOCKER=1
+        ;;
+        --keep-cache)
+            KEEP_CACHE=1
+        ;;
+    esac
+done
+
+# Create the build cache
+docker volume inspect $BUILD_CACHE_VOLUME &> /dev/null || docker volume create $BUILD_CACHE_VOLUME &> /dev/null
+# Create the dist folder
+mkdir -p $DIR/dist
+
+test $TEST -eq 1 && cmtest
+test $PACKAGE -eq 1 && cmpackage
+test $BUILD_DOCKER -eq 1 && cmdocker
+test $KEEP_CACHE -eq 0 && docker volume rm $BUILD_CACHE_VOLUME &> /dev/null
