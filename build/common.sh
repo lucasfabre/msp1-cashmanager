c9b66c907755c1e86cac1c5bbcbd42b985f6576a
Lucas Fabre <fabre.lucas@outlook.com>
2019-10-08 17:00:44 +0200

    ci(github): added github actions

--- build/common.sh
+++ build/common.sh
@@ -1,9 +1,10 @@
 set -e
 
-source $DIR/build/.env
+test -f $DIR/build/.env && source $DIR/build/.env
 
 TEST=0
 PACKAGE=0
+BUILD=0
 BUILD_DOCKER=0
 KEEP_CACHE=0
 BUILD_CACHE_VOLUME=cashmanager-buildcache
@@ -15,8 +16,11 @@ for i in "$@"
 do
     case $i in
         help)
-            print_help
+            cmhelp
             exit 0
+        ;;
+        build)
+            BUILD=1
         ;;
         test)
             TEST=1
@@ -38,6 +42,7 @@ docker volume inspect $BUILD_CACHE_VOLUME &> /dev/null || docker volume create $
 # Create the dist folder
 mkdir -p $DIR/dist
 
+test $BUILD -eq 1 && cmbuild
 test $TEST -eq 1 && cmtest
 test $PACKAGE -eq 1 && cmpackage
 test $BUILD_DOCKER -eq 1 && cmdocker
