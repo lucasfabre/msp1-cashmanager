96365f062ffd86ea105a2c0557c20bc3fada4f12
jordan <jordan.descoteaux@epitech.eu>
2019-10-08 17:23:22 +0200

    NFC fonctionnel (prise en charge carte type A)

--- application/app/src/main/java/fr/cashmanager/MainActivity.kt
+++ application/app/src/main/java/fr/cashmanager/MainActivity.kt
@@ -1,12 +1,40 @@
 package fr.cashmanager
 
+import android.nfc.NfcAdapter
+import android.nfc.Tag
+import android.nfc.tech.IsoDep
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NfcAdapter.ReaderCallback {
+    private var nfcAdapter: NfcAdapter? = null
+    private var textView: TextView? = null
+    override fun onTagDiscovered(tag: Tag?) {
+        val isoDep = IsoDep.get(tag)
+        isoDep.connect()
+        runOnUiThread { textView?.append("\nCard Response: "
+                + Utils.toHex(isoDep.tag.id)) }
+        isoDep.close()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
+        textView = findViewById(R.id.textView)
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        nfcAdapter?.enableReaderMode(this, this,
+            NfcAdapter.FLAG_READER_NFC_A or
+                    NfcAdapter.FLAG_READER_SKIP_NDEF_CHECK,
+            null)
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        nfcAdapter?.disableReaderMode(this)
     }
 }
