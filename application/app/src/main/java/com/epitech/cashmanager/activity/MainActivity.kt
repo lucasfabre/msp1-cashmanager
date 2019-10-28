0809dcf2ff2914b550795177ad3af9e5c43aa98c
jordan <jordan.descoteaux@epitech.eu>
2019-10-28 15:28:47 +0100

    feat(add): Add implementation of product and shopping cart

---
+++ application/app/src/main/java/com/epitech/cashmanager/activity/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.epitech.cashmanager.activity
+
+import android.content.Intent
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.epitech.cashmanager.R
+import com.epitech.cashmanager.services.ShoppingCart
+import io.paperdb.Paper
+import kotlinx.android.synthetic.main.fragment_home.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Paper.init(this)
+        setContentView(R.layout.fragment_home)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_market,
+                R.id.navigation_settings
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+        cart_size.text = ShoppingCart.getShoppingCartSize().toString()
+        showCart.setOnClickListener {
+            val intent = Intent(this, ShoppingCartActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
--- application/app/src/main/java/com/epitech/cashmanager/fragments/MarketFragment.kt
+++ application/app/src/main/java/com/epitech/cashmanager/fragments/MarketFragment.kt
@@ -1,104 +1,43 @@
 package com.epitech.cashmanager.fragments
 
-import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.epitech.cashmanager.R
-import com.epitech.cashmanager.fragments.adapter.MyMarketRecyclerViewAdapter
+import com.epitech.cashmanager.beans.Product
+import com.epitech.cashmanager.models.adapter.ProductAdapter
+import kotlinx.android.synthetic.main.fragment_market.*
 
-import com.epitech.cashmanager.dummy.DummyContent
-import com.epitech.cashmanager.dummy.DummyContent.DummyItem
-
-/**
- * A fragment representing a list of Items.
- * Activities containing this fragment MUST implement the
- * [MarketFragment.OnListFragmentInteractionListener] interface.
- */
 class MarketFragment : Fragment() {
 
-    // TODO: Customize parameters
-    private var columnCount = 1
+    private lateinit var productAdapter: ProductAdapter
 
-    private var listener: OnListFragmentInteractionListener? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        arguments?.let {
-            columnCount = it.getInt(ARG_COLUMN_COUNT)
-        }
+    private var products = ArrayList<Product>()
+    companion object {
+        fun newInstance() = MarketFragment()
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_market_list, container, false)
-
-        // Set the adapter
-        if (view is RecyclerView) {
-            with(view) {
-                layoutManager = when {
-                    columnCount <= 1 -> LinearLayoutManager(context)
-                    else -> GridLayoutManager(context, columnCount)
-                }
-                adapter = MyMarketRecyclerViewAdapter(
-                    DummyContent.ITEMS,
-                    listener
-                )
-            }
-        }
-        return view
+        return inflater.inflate(R.layout.fragment_market, container, false)
     }
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is OnListFragmentInteractionListener) {
-            listener = context
-        } else {
-            throw RuntimeException(context.toString() + " must implement OnListFragmentInteractionListener")
-        }
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        products_recyclerview.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        getProducts()
     }
-
-    override fun onDetach() {
-        super.onDetach()
-        listener = null
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson
-     * [Communicating with Other Fragments](http://developer.android.com/training/basics/fragments/communicating.html)
-     * for more information.
-     */
-    interface OnListFragmentInteractionListener {
-        // TODO: Update argument type and name
-        fun onListFragmentInteraction(item: DummyItem?)
-    }
-
-    companion object {
-
-        // TODO: Customize parameter argument names
-        const val ARG_COLUMN_COUNT = "column-count"
-
-        // TODO: Customize parameter initialization
-        @JvmStatic
-        fun newInstance(columnCount: Int) =
-            MarketFragment().apply {
-                arguments = Bundle().apply {
-                    putInt(ARG_COLUMN_COUNT, columnCount)
-                }
-            }
+    private fun getProducts() {
+        val product = Product("une description banal", 1, "Asus Rog", "1700", R.drawable.laptop)
+        val product2 = Product("une description banal", 2, "Vin Rouge", "70", R.drawable.wine_bottle)
+        products.add(product)
+        products.add(product2)
+        productAdapter = context?.let { ProductAdapter(it, products) }!!
+        products_recyclerview.adapter = productAdapter
     }
 }
