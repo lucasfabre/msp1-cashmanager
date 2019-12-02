10b544baeccf4c0e24f9568080f9d2bd8a699782
jordan <jordan.descoteaux@epitech.eu>
2019-12-02 16:20:04 +0100

    feat(app): Ajout de méthode transaction back nfc/qr code pour test

--- application/app/src/main/java/com/epitech/cashmanager/activity/NfcActivity.kt
+++ application/app/src/main/java/com/epitech/cashmanager/activity/NfcActivity.kt
@@ -4,10 +4,20 @@ import android.nfc.NfcAdapter
 import android.nfc.Tag
 import android.nfc.tech.IsoDep
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.epitech.cashmanager.R
+import com.epitech.cashmanager.services.ShoppingCartService
+import com.epitech.cashmanager.services.SocketService
 import com.epitech.cashmanager.tools.Utils
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.node.ObjectNode
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.sdsmdg.tastytoast.TastyToast
 import kotlinx.android.synthetic.main.activity_nfcpay.*
+import org.json.JSONArray
+import org.json.JSONObject
 
 /**
  * NfcActivity
@@ -18,8 +28,10 @@ import kotlinx.android.synthetic.main.activity_nfcpay.*
  */
 
 class NfcActivity : AppCompatActivity(), NfcAdapter.ReaderCallback {
-
+    private val mapper = ObjectMapper().registerModule(KotlinModule())
     private var nfcAdapter: NfcAdapter? = null
+    private var socketService: SocketService = SocketService()
+    private val validateErrors: JSONObject = JSONObject()
 
     override fun onTagDiscovered(tag: Tag?) {
         val isoDep = IsoDep.get(tag)
@@ -30,8 +42,41 @@ class NfcActivity : AppCompatActivity(), NfcAdapter.ReaderCallback {
                         + Utils.toHex(isoDep.tag.id)
             )
             //TODO: place card processing here
+            val errors: JSONArray = JSONArray()
+            val totalPrice = ShoppingCartService.getCart()
+                .fold(0.toDouble()) { acc, cartItem -> acc + cartItem.quantity.times(cartItem.product.price!!.toDouble()) }
+            var params: ObjectNode = mapper.createObjectNode()
+            params.put("CreditorAccountId", "acc2")
+            params.put("Amount", totalPrice)
+            socketService.sendRCPFormatData("StartTransaction", params, 1)
+            socketService.getJsonRcpObject()
+            params = mapper.createObjectNode()
+            params.put("DebtorAccountId", "acc1")
+            socketService.sendRCPFormatData("ValidateAndProcessTransaction", params, 2)
+            val deserializedRep2 = socketService.getJsonRcpObject()
+            if (deserializedRep2.get("result").asText("success")  == "false") {
+                errors.put("Payment denied")
+            }
+            validateErrors.put("errors", errors)
+            val jsonArray = validateErrors.getJSONArray("errors")
+            val errorsBuild: StringBuilder = java.lang.StringBuilder()
+            if (validateErrors.getJSONArray("errors") != null && jsonArray.length() > 0) {
+                for (i in 0 until jsonArray.length()) {
+                    errorsBuild.append(jsonArray.getString(i))
+                    if ((i + 1) != jsonArray.length()) {
+                        errorsBuild.append("\n")
+                    }
+                }
+                TastyToast.makeText(
+                    NFCView.context,
+                    errorsBuild.toString(),
+                    TastyToast.LENGTH_SHORT,
+                    TastyToast.ERROR
+                ).setGravity(Gravity.BOTTOM, 0, 150)
+                validateErrors.remove("errors")
+            }
+            isoDep.close()
         }
-        isoDep.close()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
